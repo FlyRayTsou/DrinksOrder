2e3fb23fa186f729d4846e68ce9106932110dc0c
Ray Tsou <ray.tsou@ilong-termcare.com>
2019-10-11 10:59:11 +0800

    Feature : delete order function and adjust view

--- assets/drink.js
+++ assets/drink.js
@@ -20,6 +20,10 @@ var drinkAct = {
         drinkAct.renderOrders();
         $("#editOrderModal").modal("hide");
     },
+    deleOrder: function(item) {
+        orderList.splice(item, 1);
+        drinkAct.renderOrders();
+    },
     editOrderModal: function(item) {
         $("#item-nums").val(item);
         $("#edit-order-name").val($("#" + item + "-title").text());
@@ -31,7 +35,7 @@ var drinkAct = {
         $("#order-part").empty();
         var startHtml = "";
         $.each(orderList, function(i, item){     
-            startHtml = startHtml + '<div class="card card-service-lg"><div class="con"><div class="title" id="' + i + '-title">' + item.orderName + '</div><div class="text" id="' + i +'-text">' + item.orderPrice + '</div><div class="note" id="' + i + '-note">' + item.orderNote + '</div><button class="btn btn-warning" onclick="drinkAct.editOrderModal(' + i + ')">Edit</button></div></div>';
+            startHtml = startHtml + '<div class="card card-service-lg"><div class="con"><div class="pre-intro">Order Name</div><div class="title" id="' + i + '-title">' + item.orderName + '</div><div class="pre-intro">Price $</div><div class="text" id="' + i +'-text">' + item.orderPrice + '</div><div class="pre-intro">Note</div><div class="note" id="' + i + '-note">' + item.orderNote + '</div><button class="btn btn-warning" onclick="drinkAct.editOrderModal(' + i + ')">Edit</button><button class="btn btn-danger" onclick="drinkAct.deleOrder(' + i + ')">Delete</button></div></div>';
         });
         $("#order-part").append(startHtml);
     }
